f41bc126b62604e3ce960e3a281de8eb72959123
pablomartinsdev <pablo.martins1998@gmail.com>
2026-08-03 19:13:18 -0300

    feat: mongo mcp integration

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,12 @@ export const config: ModelConfig = {
   httpReferer: '',
   xTitle: 'IA Devs - Transforming Services into Tools',
   models: [
-    'arcee-ai/trinity-large-preview:free',
+    // 'arcee-ai/trinity-large-preview:free',
+    // 'nvidia/llama-nemotron-rerank-vl-1b-v2:free'
+    // 'inclusionai/ling-3.0-flash:free'
+
+    // 'poolside/laguna-s-2.1:free'
+    'cohere/north-mini-code:free'
   ],
   provider: {
     sort: {
--- src/services/mcpService.ts
+++ src/services/mcpService.ts
@@ -1,6 +1,19 @@
-export const getMCPTools = async () => {
+import { getMongoDBTool } from '../tools/mongodbTool.ts';
+import { MultiServerMCPClient } from '@langchain/mcp-adapters';
 
+export const getMCPTools = async () => {
+  const client = new MultiServerMCPClient({
+    mcpServers: {
+      ...getMongoDBTool()
+    },
+    onMessage: (log, source) => {
+      console.log(`[${source.server}] ${log.data}`);
+    }
+  });
+
+  const mcpTools = await client.getTools();
 
   return [
+    ...mcpTools
   ];
 };
